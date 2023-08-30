470c3874aa6bcc47d61666d390416e43579f7103
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-08-30 21:31:44 +0700

    init base

---
+++ api-service-ms/src/main/java/com/fpoly/ooc/ApiServiceMsApplication.java
@@ -0,0 +1,13 @@
+package com.fpoly.ooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServiceMsApplication.class, args);
+    }
+
+}
---
+++ api-service-ms/src/test/java/com/fpoly/ooc/ApiServiceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpoly.ooc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

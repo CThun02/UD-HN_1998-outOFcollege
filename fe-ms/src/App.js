f8414d88aa4b879411753fa894df0e0ba7dfd1b6
tuanpa2003 <tuanpaph26902@fpt.edu.vn>
2023-09-02 08:49:06 +0700

    commit

--- fe-ms/src/App.js
+++ fe-ms/src/App.js
@@ -1,10 +1,16 @@
-import IndexAdmin from "./components/admins/IndexAdmin";
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CreateBill from './components/admins/function-SaleCounter/CreateBill';
+import GetAllBill from './components/admins/function-SaleCounter/GetAllBill';
 
 function App() {
   return (
-    <div>
-      <IndexAdmin />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/bill" Component={GetAllBill} index={true} />
+        <Route path="/create-bill" Component={CreateBill} />
+      </Routes>
+    </Router>
   );
 }
 

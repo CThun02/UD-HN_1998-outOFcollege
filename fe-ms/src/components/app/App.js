70f08439cc173838e5e8f1a2f5ea3807d83e0d53
NGUYEN CONG THUAN <thuanncph26631@fpt.edu.vn>
2023-09-04 10:22:05 +0700

    PUSH

--- fe-ms/src/components/app/App.js
+++ fe-ms/src/components/app/App.js
@@ -4,12 +4,14 @@ import ProductAdminIndex from "../admins/product-admin/ProductAdminIndex";
 import ProductAdminCreate from "../admins/product-admin/ProductAdminCreate";
 import ProductAdminUpdate from "../admins/product-admin/ProductAdminUpdate";
 import ProductSizeColorAdminUpdate from "../admins/product-admin/ProductSizeColorAdminUpdate";
+import IndexAdmin from "../admins/index/IndexAdmin";
 
 function App() {
   return (
     <div className={styles.app}>
       <BrowserRouter>
         <Routes>
+          <Route path="/controller/v1/admin" element={<IndexAdmin />} />
           <Route
             path="/controller/v1/admin/product"
             element={<ProductAdminIndex />}

85d5c2c3f9fd867b949cb86ab614fe225db9235c
NGUYEN CONG THUAN <thuanncph26631@fpt.edu.vn>
2023-10-23 20:13:03 +0700

    fix form voucher

--- ms-fe/src/components/element/voucher/VoucherList.js
+++ ms-fe/src/components/element/voucher/VoucherList.js
@@ -15,38 +15,28 @@ function VoucherList({ data, setValue }) {
 
   return (
     <Row className={styles.width600}>
-      <Col span={24}>
-        <Card bordered={true}>
-          <Row style={{ height: "100%" }}>
-            <Col span={7}>
-              <img src={image} alt={data.voucherName} />
-            </Col>
-            <Col span={15} className={styles.padding}>
-              <h3 className={styles.fontWeight500}>
-                {`Giảm ${numeral(data.voucherValue).format("0,0")}`}
-                {`${data.voucherMethod === "vnd" ? "đ" : "%"}`}
-              </h3>
-              <h3 className={styles.fontWeight400}>
-                Đơn tối thiểu {data.voucherCondition}đ{" "}
-                {`${
-                  data.voucherMethod === "%"
-                    ? "Giảm tối đa " + data.voucherValueMax + "đ"
-                    : ""
-                }`}
-              </h3>
-              <h4 className={`${styles.fontWeight400} ${styles.color}`}>
-                HSD: {moment(data.endDate).format("DD.MM.YYYY")}
-              </h4>
-            </Col>
-            <Col span={2} className={styles.flex}>
-              <Radio
-                value={data}
-                key={data.voucherId}
-                onClick={handleOnClick}
-              />
-            </Col>
-          </Row>
-        </Card>
+      <Col span={7}>
+        <img src={image} alt={data.voucherName} />
+      </Col>
+      <Col span={15} className={styles.padding}>
+        <h3 className={styles.fontWeight500}>
+          {`Giảm ${numeral(data.voucherValue).format("0,0")}`}
+          {`${data.voucherMethod === "vnd" ? "đ" : "%"}`}
+        </h3>
+        <h3 className={styles.fontWeight400}>
+          Đơn tối thiểu {data.voucherCondition}đ{" "}
+          {`${
+            data.voucherMethod === "%"
+              ? "Giảm tối đa " + data.voucherValueMax + "đ"
+              : ""
+          }`}
+        </h3>
+        <h4 className={`${styles.fontWeight400} ${styles.color}`}>
+          HSD: {moment(data.endDate).format("DD.MM.YYYY")}
+        </h4>
+      </Col>
+      <Col span={2} className={styles.flex}>
+        <Radio value={data} key={data.voucherId} onClick={handleOnClick} />
       </Col>
     </Row>
   );

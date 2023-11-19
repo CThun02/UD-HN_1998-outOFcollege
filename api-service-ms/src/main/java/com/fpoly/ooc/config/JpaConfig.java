927155edeec2b015571ba9bce9f2fcc08d881005
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-11-19 08:44:31 +0700

    ms-fix: customer don't login when order product error

--- api-service-ms/src/main/java/com/fpoly/ooc/config/JpaConfig.java
+++ api-service-ms/src/main/java/com/fpoly/ooc/config/JpaConfig.java
@@ -6,7 +6,9 @@ import com.fpoly.ooc.dto.UserDTO;
 import com.fpoly.ooc.exception.LoginException;
 import com.fpoly.ooc.exception.NotFoundException;
 import com.fpoly.ooc.repository.AccountRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +16,10 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Configuration
@@ -26,15 +31,26 @@ public class JpaConfig {
     public AuditorAware<String> auditorProvider() {
         return () -> {
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+            String uri = request.getRequestURI();
+            log.info("Authentication: " + uri);
 
-            if (authentication == null || !authentication.isAuthenticated()) {
+            if (uri.contains("admin") && (authentication == null || !authentication.isAuthenticated())) {
                 throw new NotFoundException(ErrorCodeConfig.getMessage(Const.JWT_AUTHENTICATION));
             }
 
             log.warn("authentication: " + authentication);
             UserDTO userDTO = (UserDTO) authentication.getPrincipal();
+            String user = null;
+            if(uri.contains("client") && userDTO == null) {
+                    user = "CLIENT";
+            } else {
+                user = userDTO.getUsername() + "_" + userDTO.getFullName();
+            }
+
+
             // return username_fullName
-            return Optional.of(userDTO.getUsername() + "_" + userDTO.getFullName());
+            return Optional.of(user);
         };
     }
 }

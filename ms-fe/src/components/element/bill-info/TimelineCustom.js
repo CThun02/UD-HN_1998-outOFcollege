bb416ec73001ae6277c1a400d3c550afbf29c44c
tuanpa2003 <tuanpaph26902@fpt.edu.vn>
2023-11-28 10:14:45 +0700

    fix bug

--- ms-fe/src/components/element/bill-info/TimelineCustom.js
+++ ms-fe/src/components/element/bill-info/TimelineCustom.js
@@ -15,20 +15,20 @@ const TimelineCustom = ({ timelines, open, handleCancel }) => {
                 footer={false}
             >
                 <Timeline minEvents={6} placeholder >
+                    {timelines}
                     {timelines &&
                         timelines.map((data) => (
                             <TimelineEvent
                                 color={data.status === "0" ? "#FF0000" : "#00cc00"}
                                 icon={
-                                    data.status === "1"
-                                        ? FaRegFileAlt
-                                        : data.status === "0"
-                                            ? FaTimes
-                                            : data.status === "2"
-                                                ? FaRegFileAlt
-                                                : data.status === "3"
-                                                    ? FaTruck
-                                                    : CheckCircleOutlined
+                                    data.status === "1" ? FaRegFileAlt
+                                        : data.status === "0" ? FaTimes
+                                            : data.status === "2" ? FaRegFileAlt
+                                                : data.status === "3" ? FaTruck
+                                                    : data.status === "4" ? FaTimes
+                                                        : data.status === "5" ? FaTimes
+                                                            : data.status === "6" ? FaRegFileAlt
+                                                                : null
                                 }
                                 title={
                                     data.status === "0" ? (
@@ -41,8 +41,14 @@ const TimelineCustom = ({ timelines, open, handleCancel }) => {
                                         <h3>
                                             Đã đóng gói & <br /> đang được giao
                                         </h3>
-                                    ) : (
+                                    ) : data.status === "4" ? (
                                         <h3>Giao hàng thành công</h3>
+                                    ) : data.status === "5" ? (
+                                        <h3>Yêu cầu trả hàng</h3>
+                                    ) : data.status === "6" ? (
+                                        <h3>Trả hang thành công</h3>
+                                    ) : (
+                                        <h3>.</h3>
                                     )
                                 }
                                 subtitle={data.createdDate}
